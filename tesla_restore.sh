f0f4ef7c29cfc38c99308d203312257979e3fd67
Andrew wilkinson <andrewjwilkinson@gmail.com>
2022-02-14 21:43:06 +0100

    Add tesla restore script.

---
+++ tesla_restore.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+source /volume1/homes/andrew/docker/secrets/postgres
+source /volume1/homes/andrew/docker/secrets/teslamate
+
+docker stop teslamate
+
+docker exec postgres psql -U $POSTGRES_USER << .
+create database teslamate;
+create user teslamate with encrypted password '$DATABASE_PASS';
+grant all privileges on database teslamate to teslamate;
+ALTER USER teslamate WITH SUPERUSER;
+.
+
+echo $1
+cat $1 | docker exec -i postgres psql -U $DATABASE_USER $DATABASE_NAME
+echo $?
+
+docker start teslamate
